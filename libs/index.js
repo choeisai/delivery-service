4219b4876b114205b0de0175df3586fb7d24beb2
Chalermrat Srinara <schalermrat@gmail.com>
2017-11-20 08:21:44 +0700

    feat(libs): Add libs endpoint

---
+++ libs/index.js
@@ -0,0 +1,9 @@
+const pathCost = require('./pathCost')
+const shortestPathCost = require('./shortestPathCost')
+const allRoutes = require('./allRoutes')
+
+module.exports = {
+  pathCost,
+  shortestPathCost,
+  allRoutes
+}
